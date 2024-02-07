21c3e7ba635e5a3c67794a1e463a8fbcd8be4f9f
Aarish <aarishgilani10@gmail.com>
2024-02-07 00:10:39 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_authors/**/*.html',
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.md',
+    './*.html',
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
